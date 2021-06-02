70d657140f02d6fa374a20569a580c4e06febda3
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2021-06-02 20:51:21 -0300

    Criando a classe PosicaoXadrez

--- JogoDeXadrezModoConsoleCSharp/JogoDeXadrezModoConsoleCSharp/Program.cs
+++ JogoDeXadrezModoConsoleCSharp/JogoDeXadrezModoConsoleCSharp/Program.cs
@@ -10,13 +10,16 @@ namespace JogoDeXadrezModoConsoleCSharp
         {
             try
             {
-                Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+                //Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
-                tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(0, 0));
-                tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(1, 3));
-                tabuleiro.ColocarPeca(new Rei(Cor.Preta, tabuleiro), new Posicao(2, 4));
+                //tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(0, 0));
+                //tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(1, 3));
+                //tabuleiro.ColocarPeca(new Rei(Cor.Preta, tabuleiro), new Posicao(2, 4));
 
-                Tela.ImprimirTabuleiro(tabuleiro);
+                //Tela.ImprimirTabuleiro(tabuleiro);
+                PosicaoXadrez posicaoXadrez = new PosicaoXadrez('c', 7);
+                Console.WriteLine(posicaoXadrez);
+                Console.WriteLine(posicaoXadrez.ToPosicao());
             }
             catch (TabuleiroException e)
             {
---
+++ JogoDeXadrezModoConsoleCSharp/JogoDeXadrezModoConsoleCSharp/Xadrez/PosicaoXadrez.cs
@@ -0,0 +1,26 @@
+﻿using JogoDeXadrezModoConsoleCSharp.tabuleiro;
+
+namespace JogoDeXadrezModoConsoleCSharp.Xadrez
+{
+    class PosicaoXadrez
+    {
+        public int Linha { get; set; }
+        public char Coluna { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        public Posicao ToPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return $"{Coluna}{Linha}";
+        }
+    }
+}
