6078801aa3578d9002018ee66aaf57bba514dc7f
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2021-06-02 09:42:55 -0300

    Começando a implementar um método que imprime o tabuleiro

--- JogoDeXadrezModoConsoleCSharp/JogoDeXadrezModoConsoleCSharp/Program.cs
+++ JogoDeXadrezModoConsoleCSharp/JogoDeXadrezModoConsoleCSharp/Program.cs
@@ -9,6 +9,8 @@ namespace JogoDeXadrezModoConsoleCSharp
         {
             Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
+            Tela.ImprimirTabuleiro(tabuleiro);
+
             Console.ReadLine();
         }
     }
