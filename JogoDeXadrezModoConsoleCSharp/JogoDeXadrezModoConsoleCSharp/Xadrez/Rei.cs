9d7a4bc9aa5eef8688ef61c4319f8634ec07c311
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2021-06-03 20:56:41 -0300

    Implementando os movimentos possíveis do rei

--- JogoDeXadrezModoConsoleCSharp/JogoDeXadrezModoConsoleCSharp/Xadrez/Rei.cs
+++ JogoDeXadrezModoConsoleCSharp/JogoDeXadrezModoConsoleCSharp/Xadrez/Rei.cs
@@ -8,6 +8,78 @@ namespace JogoDeXadrezModoConsoleCSharp.Xadrez
         {
         }
 
+        private bool PodeMover(Posicao posicao)
+        {
+            Peca peca = Tabuleiro.Peca(posicao);
+
+            return peca == null || peca.Cor != Cor;
+        }
+
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] matriz = new bool[Tabuleiro.Linhas, Tabuleiro.Colunas];
+
+            Posicao posicao = new Posicao(0, 0);
+
+            // Acima
+            posicao.DefinirValores(posicao.Linha - 1, posicao.Coluna);
+            if (Tabuleiro.PosicaoValida(posicao) && PodeMover(posicao))
+            {
+                matriz[posicao.Linha, posicao.Coluna] = true;
+            }
+
+            // Nordeste
+            posicao.DefinirValores(posicao.Linha - 1, posicao.Coluna + 1);
+            if (Tabuleiro.PosicaoValida(posicao) && PodeMover(posicao))
+            {
+                matriz[posicao.Linha, posicao.Coluna] = true;
+            }
+
+            // Direita
+            posicao.DefinirValores(posicao.Linha, posicao.Coluna + 1);
+            if (Tabuleiro.PosicaoValida(posicao) && PodeMover(posicao))
+            {
+                matriz[posicao.Linha, posicao.Coluna] = true;
+            }
+
+            // Sudeste
+            posicao.DefinirValores(posicao.Linha + 1, posicao.Coluna + 1);
+            if (Tabuleiro.PosicaoValida(posicao) && PodeMover(posicao))
+            {
+                matriz[posicao.Linha, posicao.Coluna] = true;
+            }
+
+            // Abaixo
+            posicao.DefinirValores(posicao.Linha + 1, posicao.Coluna);
+            if (Tabuleiro.PosicaoValida(posicao) && PodeMover(posicao))
+            {
+                matriz[posicao.Linha, posicao.Coluna] = true;
+            }
+
+            // Sudoeste
+            posicao.DefinirValores(posicao.Linha + 1, posicao.Coluna - 1);
+            if (Tabuleiro.PosicaoValida(posicao) && PodeMover(posicao))
+            {
+                matriz[posicao.Linha, posicao.Coluna] = true;
+            }
+
+            // Esquerda
+            posicao.DefinirValores(posicao.Linha, posicao.Coluna - 1);
+            if (Tabuleiro.PosicaoValida(posicao) && PodeMover(posicao))
+            {
+                matriz[posicao.Linha, posicao.Coluna] = true;
+            }
+
+            // Noroestes
+            posicao.DefinirValores(posicao.Linha - 1, posicao.Coluna - 1);
+            if (Tabuleiro.PosicaoValida(posicao) && PodeMover(posicao))
+            {
+                matriz[posicao.Linha, posicao.Coluna] = true;
+            }
+
+            return matriz;
+        }
+
         public override string ToString()
         {
             return "R";
