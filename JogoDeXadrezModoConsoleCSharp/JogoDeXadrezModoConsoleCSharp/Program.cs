bb4b66e1c17f235c81f13d2de8c40ad74804144d
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2021-06-02 21:22:50 -0300

    Melhorando a impressão do Tabuleiro

--- JogoDeXadrezModoConsoleCSharp/JogoDeXadrezModoConsoleCSharp/Program.cs
+++ JogoDeXadrezModoConsoleCSharp/JogoDeXadrezModoConsoleCSharp/Program.cs
@@ -10,16 +10,14 @@ namespace JogoDeXadrezModoConsoleCSharp
         {
             try
             {
-                //Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+                Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
-                //tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(0, 0));
-                //tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(1, 3));
-                //tabuleiro.ColocarPeca(new Rei(Cor.Preta, tabuleiro), new Posicao(2, 4));
+                tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(0, 0));
+                tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(1, 3));
+                tabuleiro.ColocarPeca(new Rei(Cor.Preta, tabuleiro), new Posicao(2, 4));
+                tabuleiro.ColocarPeca(new Rei(Cor.Branca, tabuleiro), new Posicao(3, 5));
 
-                //Tela.ImprimirTabuleiro(tabuleiro);
-                PosicaoXadrez posicaoXadrez = new PosicaoXadrez('c', 7);
-                Console.WriteLine(posicaoXadrez);
-                Console.WriteLine(posicaoXadrez.ToPosicao());
+                Tela.ImprimirTabuleiro(tabuleiro);
             }
             catch (TabuleiroException e)
             {
--- JogoDeXadrezModoConsoleCSharp/JogoDeXadrezModoConsoleCSharp/Tela.cs
+++ JogoDeXadrezModoConsoleCSharp/JogoDeXadrezModoConsoleCSharp/Tela.cs
@@ -9,6 +9,7 @@ namespace JogoDeXadrezModoConsoleCSharp
         {
             for (int i = 0; i < tabuleiro.Linhas; i++)
             {
+                Console.Write($" {8 - i}   ");
                 for (int j = 0; j < tabuleiro.Colunas; j++)
                 {
                     if (tabuleiro.Peca(i, j) == null)
@@ -17,11 +18,30 @@ namespace JogoDeXadrezModoConsoleCSharp
                     }
                     else
                     {
-                        Console.Write($"{tabuleiro.Peca(i, j)} ");
+                        ImprimirPeca(tabuleiro.Peca(i, j));
+                        Console.Write(" ");
                     }
                 }
 
                 Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("     A B C D E F G H");
+        }
+
+        public static void ImprimirPeca(Peca peca)
+        {
+            if (peca.Cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
