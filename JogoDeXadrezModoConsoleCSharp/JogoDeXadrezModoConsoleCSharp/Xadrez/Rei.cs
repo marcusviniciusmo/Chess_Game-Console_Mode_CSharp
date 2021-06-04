3d47c4e00c35d022c7625f94e908605984f4cd1b
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2021-06-04 16:41:38 -0300

    Começando a implementar a jogada especial Roque Pequeno

--- JogoDeXadrezModoConsoleCSharp/JogoDeXadrezModoConsoleCSharp/Xadrez/Rei.cs
+++ JogoDeXadrezModoConsoleCSharp/JogoDeXadrezModoConsoleCSharp/Xadrez/Rei.cs
@@ -4,8 +4,11 @@ namespace JogoDeXadrezModoConsoleCSharp.Xadrez
 {
     class Rei : Peca
     {
-        public Rei(Cor cor, Tabuleiro tabuleiro) : base(cor, tabuleiro)
+        private PartidaDeXadrez Partida;
+
+        public Rei(Cor cor, Tabuleiro tabuleiro, PartidaDeXadrez partida) : base(cor, tabuleiro)
         {
+            Partida = partida;
         }
 
         private bool PodeMover(Posicao posicao)
@@ -13,6 +16,13 @@ namespace JogoDeXadrezModoConsoleCSharp.Xadrez
             Peca peca = Tabuleiro.Peca(posicao);
 
             return peca == null || peca.Cor != Cor;
+        }
+
+        private bool TesteTorreParaRoque(Posicao posicao)
+        {
+            Peca peca = Tabuleiro.Peca(posicao);
+
+            return peca != null && peca is Torre && peca.Cor == Cor && peca.QtdMovimentos == 0;
         }
 
         public override bool[,] MovimentosPossiveis()
@@ -75,6 +85,24 @@ namespace JogoDeXadrezModoConsoleCSharp.Xadrez
             if (Tabuleiro.PosicaoValida(posicao) && PodeMover(posicao))
             {
                 matriz[posicao.Linha, posicao.Coluna] = true;
+            }
+
+            // #jogadaespecial Roque
+            if (QtdMovimentos == 0 && !Partida.Xeque)
+            {
+                // #jogadaespecial Roque Pequeno
+                Posicao posicaoTorreRoquePequeno = new Posicao(Posicao.Linha, Posicao.Coluna + 3);
+
+                if (TesteTorreParaRoque(posicaoTorreRoquePequeno))
+                {
+                    Posicao p1 = new Posicao(Posicao.Linha, Posicao.Coluna + 1);
+                    Posicao p2 = new Posicao(Posicao.Linha, Posicao.Coluna + 2);
+
+                    if (Tabuleiro.Peca(p1) == null && Tabuleiro.Peca(p2) == null)
+                    {
+                        matriz[Posicao.Linha, Posicao.Coluna + 2] = true;
+                    }
+                }
             }
 
             return matriz;
