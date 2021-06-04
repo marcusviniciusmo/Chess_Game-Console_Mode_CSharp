43c619831e2d3df5a09f3b2362c6d4fde3e24ec3
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2021-06-04 08:34:53 -0300

    Testando o controle de peças capturadas

--- JogoDeXadrezModoConsoleCSharp/JogoDeXadrezModoConsoleCSharp/Program.cs
+++ JogoDeXadrezModoConsoleCSharp/JogoDeXadrezModoConsoleCSharp/Program.cs
@@ -17,10 +17,7 @@ namespace JogoDeXadrezModoConsoleCSharp
                     try
                     {
                         Console.Clear();
-                        Tela.ImprimirTabuleiro(partida.Tabuleiro);
-                        Console.WriteLine();
-                        Console.WriteLine($"Turno: {partida.Turno}");
-                        Console.WriteLine($"Aguardando jogada: {partida.JogadorAtual}");
+                        Tela.ImprimirPartida(partida);
 
                         Console.WriteLine();
                         Console.Write("Origem: ");
--- JogoDeXadrezModoConsoleCSharp/JogoDeXadrezModoConsoleCSharp/Tela.cs
+++ JogoDeXadrezModoConsoleCSharp/JogoDeXadrezModoConsoleCSharp/Tela.cs
@@ -1,11 +1,49 @@
 ﻿using JogoDeXadrezModoConsoleCSharp.tabuleiro;
 using JogoDeXadrezModoConsoleCSharp.Xadrez;
 using System;
+using System.Collections.Generic;
 
 namespace JogoDeXadrezModoConsoleCSharp
 {
     class Tela
     {
+        public static void ImprimirPartida(PartidaDeXadrez partida)
+        {
+            ImprimirTabuleiro(partida.Tabuleiro);
+            Console.WriteLine();
+            ImprimirPecasCapturadas(partida);
+            Console.WriteLine();
+            Console.WriteLine($"Turno: {partida.Turno}");
+            Console.WriteLine($"Aguardando jogada: {partida.JogadorAtual}");
+        }
+
+        public static void ImprimirPecasCapturadas(PartidaDeXadrez partida)
+        {
+            Console.WriteLine("Peças capturadas:");
+            Console.Write("Branca: ");
+            ImprimirConjunto(partida.PecasCapturadas(Cor.Branca));
+            
+            ConsoleColor corPeca = Console.ForegroundColor;
+            ConsoleColor corFundo = Console.BackgroundColor;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.Write("Pretas: ");
+            ImprimirConjunto(partida.PecasCapturadas(Cor.Preta));
+            Console.ForegroundColor = corPeca;
+            Console.BackgroundColor = corFundo;
+        }
+
+        public static void ImprimirConjunto(HashSet<Peca> conjunto)
+        {
+            Console.Write("[");
+
+            foreach(Peca peca in conjunto)
+            {
+                Console.Write($"{peca}, ");
+            }
+            Console.WriteLine("]");
+        }
+
         public static void ImprimirTabuleiro(Tabuleiro tabuleiro)
         {
             for (int i = 0; i < tabuleiro.Linhas; i++)
