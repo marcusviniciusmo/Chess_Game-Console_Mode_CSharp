476faf9f978e1edb4851b486ef5607b62471f535
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2021-06-03 21:14:12 -0300

    Implementando os movimentos possíveis da torre

--- JogoDeXadrezModoConsoleCSharp/JogoDeXadrezModoConsoleCSharp/Xadrez/Torre.cs
+++ JogoDeXadrezModoConsoleCSharp/JogoDeXadrezModoConsoleCSharp/Xadrez/Torre.cs
@@ -8,6 +8,74 @@ namespace JogoDeXadrezModoConsoleCSharp.Xadrez
         {
         }
 
+        private bool PodeMover(Posicao posicao)
+        {
+            Peca peca = Tabuleiro.Peca(posicao);
+
+            return peca == null || peca.Cor != Cor;
+        }
+
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] matriz = new bool[Tabuleiro.Linhas, Tabuleiro.Colunas];
+
+            Posicao posicao = new Posicao(0, 0);
+
+            // Acima
+            posicao.DefinirValores(posicao.Linha - 1, posicao.Coluna);
+            while (Tabuleiro.PosicaoValida(posicao) && PodeMover(posicao))
+            {
+                matriz[posicao.Linha, posicao.Coluna] = true;
+                if (Tabuleiro.Peca(posicao) != null && Tabuleiro.Peca(posicao).Cor != Cor)
+                {
+                    break;
+                }
+
+                posicao.Linha -= -1;
+            }
+
+            // Abaixo
+            posicao.DefinirValores(posicao.Linha + 1, posicao.Coluna);
+            while (Tabuleiro.PosicaoValida(posicao) && PodeMover(posicao))
+            {
+                matriz[posicao.Linha, posicao.Coluna] = true;
+                if (Tabuleiro.Peca(posicao) != null && Tabuleiro.Peca(posicao).Cor != Cor)
+                {
+                    break;
+                }
+
+                posicao.Linha += 1;
+            }
+
+            // Direita
+            posicao.DefinirValores(posicao.Linha, posicao.Coluna + 1);
+            while (Tabuleiro.PosicaoValida(posicao) && PodeMover(posicao))
+            {
+                matriz[posicao.Linha, posicao.Coluna] = true;
+                if (Tabuleiro.Peca(posicao) != null && Tabuleiro.Peca(posicao).Cor != Cor)
+                {
+                    break;
+                }
+
+                posicao.Coluna += 1;
+            }
+
+            // Esquerda
+            posicao.DefinirValores(posicao.Linha, posicao.Coluna - 1);
+            while (Tabuleiro.PosicaoValida(posicao) && PodeMover(posicao))
+            {
+                matriz[posicao.Linha, posicao.Coluna] = true;
+                if (Tabuleiro.Peca(posicao) != null && Tabuleiro.Peca(posicao).Cor != Cor)
+                {
+                    break;
+                }
+
+                posicao.Coluna -= -1;
+            }
+
+            return matriz;
+        }
+
         public override string ToString()
         {
             return "T";
